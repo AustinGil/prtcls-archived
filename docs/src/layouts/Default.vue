f51ea7170c9eff2f0d930edafb60c428e004fec1
Austin Gil <austin@stegosource.com>
2019-12-31 06:39:50 -0800

    Adding gridsome for docs

---
+++ docs/src/layouts/Default.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="font-sans mx-12">
+    <header class="flex justify-between align-center mb-5 py-7">
+      <strong>
+        <g-link to="/">{{ $static.metadata.siteName }}</g-link>
+      </strong>
+    </header>
+    <slot />
+  </div>
+</template>
+
+<static-query>
+query {
+  metadata {
+    siteName
+  }
+}
+</static-query>
