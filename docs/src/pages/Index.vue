1e2fa26a2d96eadb02a6dc7df2b8cb9b0974ed92
Austin Gil <austin@stegosource.com>
2019-12-31 06:43:05 -0800

    Fix html error

--- docs/src/pages/Index.vue
+++ docs/src/pages/Index.vue
@@ -50,7 +50,7 @@
         <span class="visually-hidden">
           .visually-hidden { visibility: hidden; }
         </span>
-        <-- .visually-hidden { border: 0; clip: rect(0 0 0 0); height: 1px; margin: -1px; overflow:
+        &lt;-- .visually-hidden { border: 0; clip: rect(0 0 0 0); height: 1px; margin: -1px; overflow:
         hidden; padding: 0; position: absolute; width: 1px; }
       </p>
     </section>
