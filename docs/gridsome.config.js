6bdfe13836f43c7328253bcf8f20aac82ee29aa1
Austin Gil <austin@stegosource.com>
2020-01-07 09:35:27 -0800

    Temporarily disable purgecss

--- docs/gridsome.config.js
+++ docs/gridsome.config.js
@@ -1,14 +1,14 @@
-const glob = require("glob");
-const PurgecssPlugin = require("purgecss-webpack-plugin");
+// const glob = require("glob");
+// const PurgecssPlugin = require("purgecss-webpack-plugin");
 
-const purgecss = new PurgecssPlugin({
-  paths: glob.sync(`./src/**/*.vue`)
-});
+// const purgecss = new PurgecssPlugin({
+//   paths: glob.sync(`./src/**/*.vue`)
+// });
 
 module.exports = {
   siteName: "Particles CSS",
   plugins: [],
   configureWebpack: {
-    plugins: [...(process.env.NODE_ENV === "production" ? [purgecss] : [])]
+    // plugins: [...(process.env.NODE_ENV === "production" ? [purgecss] : [])]
   }
 };
