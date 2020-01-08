9e0029dc45584c228dd5fcbae5dd3e57779a293e
Austin Gil <austin@stegosource.com>
2020-01-08 09:01:14 -0800

    Fix an SSR issue for build

--- docs/src/layouts/Default.vue
+++ docs/src/layouts/Default.vue
@@ -1,25 +1,27 @@
 <template>
   <div class="max-w-4 my-10 mx-auto px-4">
-    <button
-      @click="drawer = !drawer"
-      class="fixed z-1 right-0 mr-3 radius-1 p-4 size-3 color-white bg-purple-5"
-    >
-      Menu
-    </button>
+    <ClientOnly>
+      <button
+        @click="drawer = !drawer"
+        class="fixed z-1 right-0 mr-3 radius-1 p-4 size-3 color-white bg-purple-5"
+      >
+        Menu
+      </button>
 
-    <VDrawer v-model="drawer">
-      <nav class="p-5">
-        <h3 class="text-center">
-          <g-link to="/" class="no-line">Particles CSS</g-link>
-        </h3>
-        <h3>Rules</h3>
-        <ul class="nav">
-          <li v-for="(link, i) in $options.links" :key="i">
-            <g-link :to="link.to">{{ link.text }}</g-link>
-          </li>
-        </ul>
-      </nav>
-    </VDrawer>
+      <VDrawer v-model="drawer">
+        <nav class="p-5">
+          <h3 class="text-center">
+            <g-link to="/" class="no-line">Particles CSS</g-link>
+          </h3>
+          <h3>Rules</h3>
+          <ul class="nav">
+            <li v-for="(link, i) in $options.links" :key="i">
+              <g-link :to="link.to">{{ link.text }}</g-link>
+            </li>
+          </ul>
+        </nav>
+      </VDrawer>
+    </ClientOnly>
 
     <slot />
   </div>
