21c29f6332b991ff721b6227de13603e34edc200
Austin Gil <austin@stegosource.com>
2020-03-08 09:18:01 -0700

    Finishing up some grid  docs and fixing some logic

--- docs/src/pages/Grid.vue
+++ docs/src/pages/Grid.vue
@@ -25,6 +25,8 @@
             'grid-template-columns: repeat(auto-fit, minmax(0, 1fr))',
           '.columns-{1-12}': 'grid-template-columns: {1-12};',
           '.rows-{1-12}': 'grid-template-rows: {1-12};',
+          '.flow-column': 'grid-auto-flow: column;',
+          '.flow-row': 'grid-auto-flow: row;',
           '.gap-0': 'grid-column-gap: 0;\ngrid-row-gap: 0;',
           '.column-gap-0': 'grid-column-gap: 0;',
           '.row-gap-0': 'grid-row-gap: 0;',
@@ -33,8 +35,8 @@
           '.column-gap-{1-8}': 'grid-column-gap: {$gaps[1-8]};',
           '.row-gap-{1-8}': 'grid-row-gap: {$gaps[1-8]};',
           '.column-span-full': 'grid-column: 1/-1;',
-          '.row-span-full': 'grid-row: 1/-1;',
           '.column-span-{1-12}': 'grid-column: {1-12};',
+          '.row-span-full': 'grid-row: 1/-1;',
           '.row-span-{1-12}': 'grid-row: {1-12};'
         }"
       />
@@ -45,12 +47,19 @@
       <p>Sets the display property to <code>grid</code>.</p>
     </template>
 
-    <template #columns-auto>
-      <h2>.grid.columns-auto</h2>
-      <p>Sets a grid container to create rows that automatically size.</p>
+    <template #flow>
+      <h2>.flow-{row|column}</h2>
+      <p>Sets the <code>grid-auto-flow</code> property.</p>
       <PExample
         :html="
-          `<div class=&quot;grid columns-auto&quot;>
+          `<small>.grid.flow-column</small>
+<div class=&quot;grid flow-column mb-4&quot;>
+  <div class=&quot;border-1 border-gray-3 p-4 bg-gray-1&quot;></div>
+  <div class=&quot;border-1 border-gray-3 p-4 bg-gray-1&quot;></div>
+  <div class=&quot;border-1 border-gray-3 p-4 bg-gray-1&quot;></div>
+</div>
+<small>.grid.flow-row</small>
+<div class=&quot;grid flow-row&quot;>
   <div class=&quot;border-1 border-gray-3 p-4 bg-gray-1&quot;></div>
   <div class=&quot;border-1 border-gray-3 p-4 bg-gray-1&quot;></div>
   <div class=&quot;border-1 border-gray-3 p-4 bg-gray-1&quot;></div>
@@ -59,14 +68,14 @@
       />
     </template>
 
-    <!-- <template>
-      <p>Grid with strictly defined columns (supports 1-8 columns)</p>
+    <template #columns>
+      <h2>Columns</h2>
+      <h3>.columns-auto</h3>
+      <p>Creates a grid container with columns that automatically size.</p>
       <PExample
         :html="
-          `<div class=&quot;grid columns-4&quot;>
-  <div class=&quot;border-1 border-gray-3 p-4 bg-gray-1&quot;></div>
-  <div class=&quot;border-1 border-gray-3 p-4 bg-gray-1&quot;></div>
-  <div class=&quot;border-1 border-gray-3 p-4 bg-gray-1&quot;></div>
+          `<small>.grid.columns-auto</small>
+<div class=&quot;grid columns-auto&quot;>
   <div class=&quot;border-1 border-gray-3 p-4 bg-gray-1&quot;></div>
   <div class=&quot;border-1 border-gray-3 p-4 bg-gray-1&quot;></div>
   <div class=&quot;border-1 border-gray-3 p-4 bg-gray-1&quot;></div>
@@ -74,20 +83,110 @@
         "
       />
 
-      <p>Grid with strictly defined rows (supports 1-8 rows)</p>
+      <h3 class="mt-8">.grid.columns-{1-12}</h3>
+      <p>Creates a grid container with 1-12 columns.</p>
       <PExample
         :html="
-          `<div class=&quot;grid rows-4&quot;>
-  <div class=&quot;border-1 border-gray-3 p-4 bg-gray-1&quot;></div>
-  <div class=&quot;border-1 border-gray-3 p-4 bg-gray-1&quot;></div>
-  <div class=&quot;border-1 border-gray-3 p-4 bg-gray-1&quot;></div>
-  <div class=&quot;border-1 border-gray-3 p-4 bg-gray-1&quot;></div>
-  <div class=&quot;border-1 border-gray-3 p-4 bg-gray-1&quot;></div>
-  <div class=&quot;border-1 border-gray-3 p-4 bg-gray-1&quot;></div>
+          `<small>.grid.columns-2</small>
+<div class=&quot;grid columns-2 mb-4&quot;>
+  <div class=&quot;border-1 border-gray-3 p-4 bg-gray-1 text-center&quot;>1</div>
+  <div class=&quot;border-1 border-gray-3 p-4 bg-gray-1 text-center&quot;>2</div>
+  <div class=&quot;border-1 border-gray-3 p-4 bg-gray-1 text-center&quot;>3</div>
+  <div class=&quot;border-1 border-gray-3 p-4 bg-gray-1 text-center&quot;>4</div>
 </div>`
         "
       />
-    </template> -->
+    </template>
+
+    <template #rows>
+      <h2>Rows</h2>
+      <h3>.grid.rows-auto</h3>
+      <p>Creates a grid container with auto rows.</p>
+      <PExample
+        :html="
+          `<small>.grid.flow-column.rows-auto</small>
+<div class=&quot;grid flow-column rows-auto mb-4&quot;>
+  <div class=&quot;border-1 border-gray-3 p-4 bg-gray-1 text-center&quot;>1</div>
+  <div class=&quot;border-1 border-gray-3 p-4 bg-gray-1 text-center&quot;>2</div>
+  <div class=&quot;border-1 border-gray-3 p-4 bg-gray-1 text-center&quot;>3</div>
+  <div class=&quot;border-1 border-gray-3 p-4 bg-gray-1 text-center&quot;>4</div>
+</div>`
+        "
+      />
+      <h3 class="mt-8">.grid.rows-{1-12}</h3>
+      <p>Creates a grid container with 1-12 rows.</p>
+      <PExample
+        :html="
+          `<small>.grid.flow-column.rows-2</small>
+<div class=&quot;grid flow-column rows-2 mb-4&quot;>
+  <div class=&quot;border-1 border-gray-3 p-4 bg-gray-1 text-center&quot;>1</div>
+  <div class=&quot;border-1 border-gray-3 p-4 bg-gray-1 text-center&quot;>2</div>
+  <div class=&quot;border-1 border-gray-3 p-4 bg-gray-1 text-center&quot;>3</div>
+  <div class=&quot;border-1 border-gray-3 p-4 bg-gray-1 text-center&quot;>4</div>
+</div>`
+        "
+      />
+    </template>
+
+    <template #gaps>
+      <h2>Gaps</h2>
+      <h3>.grid.gap-0</h3>
+      <p>
+        Sets the <code>grid-gap</code> property to <code>0</code>. You can also
+        use <code>grid-column-gap</code> or <code>grid-row-gap</code> to set
+        their respecitve properties.
+      </p>
+      <h3>.grid.gap-{1-8}</h3>
+      <p>
+        Sets the <code>grid-gap</code> property to the corresponding value in
+        <code>$gaps</code>. You can also use <code>grid-column-gap</code> or
+        <code>grid-row-gap</code> to set their respecitve properties.
+      </p>
+      <PExample
+        :html="
+          `<div class=&quot;grid columns-2 gap-4&quot;>
+  <div class=&quot;border-1 border-gray-3 p-4 bg-gray-1 text-center&quot;>1</div>
+  <div class=&quot;border-1 border-gray-3 p-4 bg-gray-1 text-center&quot;>2</div>
+  <div class=&quot;border-1 border-gray-3 p-4 bg-gray-1 text-center&quot;>3</div>
+  <div class=&quot;border-1 border-gray-3 p-4 bg-gray-1 text-center&quot;>4</div>
+</div>`
+        "
+      />
+    </template>
+
+    <template #spans>
+      <h2>Spans</h2>
+      <h3>.column-span-full</h3>
+      <p>
+        Sets the <code>grid-column</code> property to <code>1/-1</code> so it
+        spans the full width of the grid.
+      </p>
+      <h3>.column-span-{1-12}</h3>
+      <p>
+        Sets the <code>grid-column</code> property to <code>span {1-12}</code>.
+      </p>
+      <h3>.row-span-full</h3>
+      <p>
+        Sets the <code>grid-row</code> property to <code>1/-1</code> so it spans
+        the full width of the grid.
+      </p>
+      <h3>.row-span-{1-12}</h3>
+      <p>
+        Sets the <code>grid-row</code> property to <code>span {1-12}</code>.
+      </p>
+      <PExample
+        :html="
+          `<div class=&quot;grid columns-3 gap-4&quot;>
+  <div class=&quot;column-span-full border-1 border-gray-3 p-4 bg-gray-1 text-center&quot;>1</div>
+  <div class=&quot;row-span-2 border-1 border-gray-3 p-4 bg-gray-1 text-center&quot;>2</div>
+  <div class=&quot;column-span-2 border-1 border-gray-3 p-4 bg-gray-1 text-center&quot;>3</div>
+  <div class=&quot;border-1 border-gray-3 p-4 bg-gray-1 text-center&quot;>4</div>
+</div>`
+        "
+      />
+    </template>
+
+    <!-- TODO: Add responsive example -->
   </Layout>
 </template>
 
